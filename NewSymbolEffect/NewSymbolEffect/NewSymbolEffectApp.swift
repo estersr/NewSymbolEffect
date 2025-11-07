d17a4245102b1d10662f6b04c22461b3b339c367
Esther Ramos <87035589+estersr@users.noreply.github.com>
2025-11-07 12:05:47 -0300

    Add files via upload

---
+++ NewSymbolEffect/NewSymbolEffect/NewSymbolEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSymbolEffectApp.swift
+//  NewSymbolEffect
+//
+//  Created by Esther Ramos on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct NewSymbolEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
